1309065e222f3ae5057ea681eaaf3a1b2c0a533d
Donald Johnson <donaldjamesjohnson@protonmail.com>
2020-04-15 21:56:38 -0500

    Implemented AbstractTask

--- src/AbstractTask.java
+++ src/AbstractTask.java
@@ -1,4 +1,22 @@
+public abstract class AbstractTask implements Runnable 
+{
+    String name;
+    boolean completed;
 
-public class Task {
+    public AbstractTask(String name) {
+        this.name = name;
+        this.completed = false;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted() {
+    	completed = true;
+    }
 }
